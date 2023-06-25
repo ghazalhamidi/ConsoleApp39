4f8f1d1e7d5a8ec457399ed584fd3319a8757169
Partiran <Partiran@DESKTOP-IR2LBIU>
2023-06-25 18:55:23 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project1
+{
+    class test1
+    {
+        public static void main()
+        {
+            int x = Convert.ToDouble32(Console.ReadLine());
+            int y = Convert.ToDouble32(Console.ReadLine());
+
+            double R = calc(x, y);
+            Console.WriteLine(R);
+        }//end main
+        private double calc(double x,double y)
+        {
+            double result = 0.35 * x + 0.65 * y;
+            return result;
+
+        }
+    }
+
+}
